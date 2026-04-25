67e326868b34fe4208d3198e41d90541bd433e68
Charan Shankar <83334298+charan1787@users.noreply.github.com>
2026-04-25 22:26:54 +0100

    Delete 9 - Custom PyTorch Kernel/setup.py

--- 9 - Custom PyTorch Kernel/setup.py
+++
@@ -1,7 +0,0 @@
-#include <torch/extension.h>
-
-torch::Tensor reduction_forward(torch::Tensor input);
-
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("forward", &reduction_forward, "Custom CUDA reduction");
-}
